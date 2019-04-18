8993f40dce5377240488ff34e8a1bf1aacdc94b2
Anthony Kim <anthony011@gmail.com>
2019-04-17 20:04:27 -0400

    comment cleanup

--- server.js
+++ server.js
@@ -5,17 +5,6 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const passport = require('passport');
 
-// Here we use destructuring assignment with renaming so the two variables
-// called router (from ./users and ./auth) have different names
-// For example:
-// const actorSurnames = { james: "Stewart", robert: "De Niro" };
-// const { james: jimmy, robert: bobby } = actorSurnames;
-// console.log(jimmy); // Stewart - the variable name is jimmy, not james
-// console.log(bobby); // De Niro - the variable name is bobby, not robert
-
-/* const { router } = require('./users');    
-const usersRouter = router;
-*/
 const { router: usersRouter } = require('./users');  
 const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
 const { router: eventRouter } = require('./events/router.js');  // Resume router
